c828dd5e2059d100332d80639ae528d54c9641b1
Sumit <sumit.bhanushali63@gmail.com>
2017-08-03 23:58:25 +0530

    learning python

---
+++ decorators_intro.py
@@ -0,0 +1,37 @@
+def deco(func):
+    def inner():
+        print('running inner()')
+    return inner
+
+@deco
+def target():
+    print('running target()')
+
+target() #prints running inner()
+
+registry = []
+
+def register(func):
+    print('running register(%s)' % func)
+    registry.append(func)
+    return func
+
+@register
+def f1():
+    print('running f1()')
+
+@register
+def f2():
+    print('running f2()')
+
+def f3():
+    print('running f3()')
+
+def main():
+    print('running main()')
+    print('registry ->',register)
+    f1()
+    f2()
+    f3()
+
+main() #decorators are runned first and decorated functions are run when invoked  
